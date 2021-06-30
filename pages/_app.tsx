320f9e420154ddaca5de3b588995cafc8b1a32e2
koji koyama <k.koyama37@gmail.com>
2021-06-30 23:50:47 +0900

    :beginner: ヘッダーフッターサイドメニューの仮設定。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import Header from '../components/layouts/Header'
+import Footer from '../components/layouts/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+  </>)
 }
 export default MyApp
