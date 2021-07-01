528b8bb777d6e7bc00409bc5ff20dc4a83c8fa53
koji koyama <k.koyama37@gmail.com>
2021-07-01 17:03:36 +0900

    :beginner: 各ページに共通するareaコンポーネントの作成。

---
+++ components/layouts/CodeDescriptionArea.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface Props {
+  sample?: string
+}
+
+const CodeDescriptionArea: React.FC<Props> = ({sample}) => {
+  return (
+    <>
+      CodeDescriptionArea
+    </>
+  )
+}
+
+export default CodeDescriptionArea
---
+++ components/layouts/SampleComponentStructureArea.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface Props {
+  sample?: string
+}
+
+const SampleComponentStructureArea: React.FC<Props> = ({sample}) => {
+  return (
+    <>
+      SampleComponentStructureArea
+    </>
+  )
+}
+
+export default SampleComponentStructureArea
