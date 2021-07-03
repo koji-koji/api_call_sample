d5a2888edf411155e8b6a73faac262e8f6f4e00f
koji koyama <k.koyama37@gmail.com>
2021-07-03 11:23:43 +0900

    :bug: 開発時の表示崩れの修正。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,30 @@ import type { AppProps } from 'next/app'
 import React, { useState } from 'react'
 import Header from '../components/layouts/Header'
 import Footer from '../components/layouts/Footer'
+import {MuiThemeProvider} from '@material-ui/core/styles'
+import {theme} from './theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isSidemenuOpen, setIsSidemenuOpen] = useState(true);
+    // 以下を追加する
+    // React.useEffect(() => {
+    //   const jssStyles = document.querySelector('#jss-server-side');
+    //   if (jssStyles) {
+    //     jssStyles.parentElement?.removeChild(jssStyles);
+    //   }
+    // }, []);
 
-  return (<>
-      <Header isSidemenuOpen={isSidemenuOpen} setIsSidemenuOpen={setIsSidemenuOpen} >
+    React.useEffect(() => {
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side')
+      jssStyles?.parentElement?.removeChild(jssStyles)
+    }, [])
+
+  return (
+    <>
+      <MuiThemeProvider theme={theme}>
         <Component {...pageProps} />
-        <Footer />
-      </Header>
-  </>)
+      </ MuiThemeProvider>
+    </>
+  )
 }
 export default MyApp
